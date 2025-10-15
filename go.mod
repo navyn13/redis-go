b67be0b200ac8d42998fc59cc53eddba8ab30b95
Naveen <navyn13102003@gmail.com>
2025-10-15 19:07:29 +0530

    Added: Peer connection strategy for different connections

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/navyn13/redis-go
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+)
+
+const defaultListenAddr = ":5001"
+
+type Server struct {
+	Config
+	ln        net.Listener
+	peers     map[*Peer]bool
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan []byte
+}
+
+type Config struct {
+	ListenAddr string
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	go s.loop()
+
+	slog.Info("Server Running ", "listenAddr", s.ListenAddr)
+
+	return s.acceptLoop()
+}
+
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgCh:
+			fmt.Println(rawMsg)
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+		}
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerCh <- peer
+	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
+	go peer.readLoop()
+
+}
+
+func main() {
+	server := NewServer(Config{})
+	log.Fatal(server.Start())
+}
